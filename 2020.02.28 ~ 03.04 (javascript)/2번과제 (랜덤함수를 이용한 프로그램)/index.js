292901b509a3f63753343dbdd5c8807a94f87fa9
Jangyusu <dbtnss@naver.com>
2020-02-28 10:33:20 +0900

    test

---
+++ 2020.02.28 ~ 03.04 (javascript)/2번과제 (랜덤함수를 이용한 프로그램)/index.js
@@ -0,0 +1,35 @@
+var number = document.querySelector(".number"),
+    pattern = document.querySelectorAll(".pattern"),
+    all = document.querySelectorAll("p"),
+    card = document.querySelector("div"),
+    draw = document.querySelector("input"),
+    numberList = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "J", "Q", "K"],
+    patternList = ["♠", "♥", "♣", "◆"];
+
+draw.addEventListener("click", function() {
+    card.classList.add("turn");
+    number.innerHTML = "?";
+    for (var i = 0; i < pattern.length; i++) {
+        pattern[i].innerHTML = "";
+    }
+
+    setTimeout(function() {
+        card.classList.remove("turn");
+        var numberRandom = Math.floor(Math.random() * numberList.length),
+            patternRandom = Math.floor(Math.random() * patternList.length);
+
+        number.innerHTML = numberList[numberRandom];
+
+        for (var i = 0; i < all.length; i++) {
+            if (patternList[patternRandom] == "♥" && "◆") {
+                all[i].classList.add("red");
+            } else if (patternList[patternRandom] == "♠" && "♣") {
+                all[i].classList.remove("red");
+            }
+        }
+
+        for (var i = 0; i < pattern.length; i++) {
+            pattern[i].innerHTML = patternList[patternRandom];
+        }
+    }, 500);
+});
